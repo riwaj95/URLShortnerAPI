e4f65e4ab1de34198d7f9dd9eda25d5ed036716e
ratedking95 <rk9813329259>
2024-09-16 17:59:21 +0200

    second commit and push

--- src/main/java/com/project/Task/model/UrlEntity.java
+++ src/main/java/com/project/Task/model/UrlEntity.java
@@ -1,4 +1,31 @@
 package com.project.Task.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "urls")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UrlEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String originalUrl;
+
+    @Column(unique = true, nullable = false)
+    private String shortId;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/project/Task/repository/UrlRepository.java
+++ src/main/java/com/project/Task/repository/UrlRepository.java
@@ -1,4 +1,13 @@
 package com.project.Task.repository;
 
-public interface UrlRepository {
+import com.project.Task.model.UrlEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+
+    Optional<UrlEntity> findByShortId(String shortId);
 }
